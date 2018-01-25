2f03963734c43ad8691fbb92514f52a80edce0ae
superdev807 <charles.ferria21@gmail.com>
2018-01-25 06:13:32 -0800

    Add blog model and migrate

--- backend/blogs/models.py
+++ backend/blogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    """ Blog model """
+    title = models.CharField(max_length=255, blank=False)
+    slug = models.SlugField(max_length=255, blank=False, unique=True)
+    author = models.CharField(max_length=50, blank=False)
+    location = models.CharField(max_length=50, blank=False)
+    subtitle = models.CharField(max_length=255, blank=False)
+    summary = models.TextField(blank=False)
+    body = models.TextField(blank=False)
+    tags = models.CharField(max_length=255, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('id',)
