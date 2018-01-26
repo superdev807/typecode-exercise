b21accd2ac159f581d13d11a3ead13ddba4dcddf
superdev807 <charles.ferria21@gmail.com>
2018-01-26 13:54:48 -0800

    Fix env var

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API_URL: 'http://localhost:8000/api/v1',
+  BASE_API_URL: '"http://localhost:8000/api/v1"',
 })
